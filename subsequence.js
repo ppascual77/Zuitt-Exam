6c609da9cd3e66e1ecbc083bed0b01bbec55dc63
ppascual7 <patrickgbpascual@gmail.com>
2021-06-18 14:14:04 +0800

    Updated logic to handle other test cases
    Signed-off-by: ppascual7 <patrickgbpascual@gmail.com>

--- subsequence.js
+++ subsequence.js
@@ -3,6 +3,7 @@ aefcnbtdi, ajblqcpdz -> abd, acd
 Algorithm
 1. Common subsequence -> iterate through string 1 (per character) compare against str2.
 2. If matching, get index of matched in str2 and splice str2 -> yields common substring
+3. Splice str1 too to get all common subsequence
 3. Do 1 and 2 bi-directional. 
 4. Filter result: remove duplicates and return the longest string length in the results array.
 */
@@ -12,18 +13,55 @@ function commonSubs(str1, str2) {
   arr1 = [...str1];
   arr2 = [...str2];
   const res = [];
+  var candidates = [];
 
-  arr1.map((c) => {
-    for (i = 0; i < arr2.length; i++) {
-      if (c === arr2[i]) {
-        res.push(c);
-        m = arr2.indexOf(c) + 1;
-        arr2.splice(0, m) && arr2
-        break;
+  while (arr1.length !== 0) {
+    var subsequence = [];
+    const res = []; // Reset result
+    arr2 = [...str2]; // Reset arr2
+
+    arr1.map((c) => {
+      for (i = 0; i < arr2.length; i++) {
+        if (c === arr2[i]) {
+          res.push(c);
+          m = arr2.indexOf(c) + 1;
+          arr2.splice(0, m) && arr2;
+          break;
+        }
       }
+      [subsequence.push(res.join(""))];
+    });
+
+    arr1.splice(0, 1) && arr1;
+    // Remove Duplicates and results less than 1 in length ( will not be considered as subsequence)
+    subsequence = [...new Set(subsequence)].filter((sub) => sub.length !== 1);
+
+    candidates.push(subsequence);
+  }
+
+  // Merge all results
+  const merged = [];
+
+  candidates.forEach((candidate) => merged.push(...candidate));
+
+  return [...new Set(merged)];
+}
+
+// HELPER FUNCTION: GET ALL LONGEST STR
+function allLongestStrings(inputArray) {
+  let longestLength = 0;
+  const longestWords = [];
+
+  inputArray.forEach((word) => {
+    longestLength = longestLength < word.length ? word.length : longestLength;
+  });
+
+  inputArray.forEach((word) => {
+    if (word.length === longestLength) {
+      longestWords.push(word);
     }
   });
-  return res.join("");
+  return [...new Set(longestWords)];
 }
 
 // Get common subsequence bi-directional
@@ -32,27 +70,19 @@ function longestSubs(str1, str2) {
   var longestSubsequence = [];
 
   // Case sensitivity handling and ignore spaces
-  str1 = str1.toUpperCase().replace(/\s/g, '');
-  str2 = str2.toUpperCase().replace(/\s/g, '');
+  str1 = str1.toUpperCase().replace(/\s/g, "");
+  str2 = str2.toUpperCase().replace(/\s/g, "");
 
-  commonSub1 = commonSubs(str1, str2);
-  commonSub2 = commonSubs(str2, str1);
+  commonSub1 = allLongestStrings(commonSubs(str1, str2));
+  commonSub2 = allLongestStrings(commonSubs(str2, str1));
 
-  candidate.push(commonSub1, commonSub2);
+  candidate.push(...commonSub1, ...commonSub2);
 
-  // if lengths are equal, remove possible duplicate
-  if(commonSub1.length === commonSub2.length) {
-    return longestSubsequence = [...new Set(candidate)]
-  }
+  //Remove duplicates
+  longestSubsequence = [...new Set(candidate)];
 
-  // if lengths are not equal, get the longest array
-  // Typecast to Array for result consistency
-  longestSubsequence = Array(candidate.sort((a, b) =>
-     b.length - a.length
-  )[0]);
-
-  // Typecast to Array for result consistency
-  return longestSubsequence;
+  //Get the longest
+  return allLongestStrings(longestSubsequence);
 }
 
 // Test cases
@@ -62,5 +92,6 @@ console.log("LCS:", longestSubs("str", "strrRRR"));
 console.log("LCS:", longestSubs("ABAZDC", "BACBAD"));
 console.log("LCS:", longestSubs("abcaba", "ABBA"));
 console.log("LCS:", longestSubs("AGGTAB", "GXT XAYB"));
-
-
+console.log("LCS:", longestSubs("FISH", "FOSH"));
+console.log("LCS:", longestSubs("BCDAACD", "ACDBAC"));
+console.log("LCS:", longestSubs("ABCDGH", "AEDFHR"));
